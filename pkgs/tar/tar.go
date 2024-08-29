88cdb4c7bd39607edcc624381f4161db5029852b
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2024-08-29 12:39:35 +0900

    recursive tar windows

--- pkgs/tar/tar.go
+++ pkgs/tar/tar.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func Archive(dest string, files []string) error {
@@ -19,41 +20,83 @@ func Archive(dest string, files []string) error {
 		if err != nil {
 			return err
 		}
-		size := stat.Size()
-		mode := stat.Mode()
-		modTime := stat.ModTime()
-		var fileType byte
 		if stat.IsDir() {
-			fileType = tar.TypeDir
+			if err := tarDir(tw, file, stat); err != nil {
+				return err
+			}
 		} else {
-			fileType = tar.TypeReg
+			if err := tarFile(tw, file, stat); err != nil {
+				return err
+			}
 		}
-		uid, gid := getUid(stat)
-		hdr := &tar.Header{
-			Typeflag: fileType,
-			Name:     file,
-			Mode:     int64(mode.Perm()),
-			Size:     size,
-			ModTime:  modTime,
-			Uid:      uid,
-			Gid:      gid,
-		}
-		if err := tw.WriteHeader(hdr); err != nil {
-			log.Fatal(err)
-		}
-		f, err := os.Open(file)
+	}
+	return nil
+}
+
+func tarDir(tw *tar.Writer, path string, fi os.FileInfo) error {
+	mode := fi.Mode()
+	modTime := fi.ModTime()
+	uid, gid := getUid(fi)
+	hdr := &tar.Header{
+		Typeflag: byte(tar.TypeDir),
+		Name:     path,
+		Mode:     int64(mode.Perm()),
+		ModTime:  modTime,
+		Uid:      uid,
+		Gid:      gid,
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
+		log.Fatal(err)
+	}
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		name := filepath.Join(path, entry.Name())
+		stat, err := os.Stat(name)
 		if err != nil {
 			return err
 		}
-		if _, err := io.Copy(tw, f); err != nil {
-			return err
-		}
-		if err := f.Close(); err != nil {
-			return err
+		if stat.IsDir() {
+			if err := tarDir(tw, name, stat); err != nil {
+				return err
+			}
+		} else {
+			if err := tarFile(tw, name, stat); err != nil {
+				return err
+			}
 		}
 	}
-	if err := tw.Close(); err != nil {
+	return nil
+}
+
+func tarFile(tw *tar.Writer, path string, fi os.FileInfo) error {
+	size := fi.Size()
+	mode := fi.Mode()
+	modTime := fi.ModTime()
+	uid, gid := getUid(fi)
+	hdr := &tar.Header{
+		Typeflag: byte(tar.TypeReg),
+		Name:     path,
+		Mode:     int64(mode.Perm()),
+		Size:     size,
+		ModTime:  modTime,
+		Uid:      uid,
+		Gid:      gid,
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
 		log.Fatal(err)
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(tw, f); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
 	}
 	return nil
 }
